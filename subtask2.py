e512000a69e37fcfba7ed5d9ef6ee592cde528ad
Zainab617 <Zainab.Berro@students.fh-wels.at>
2025-12-30 12:00:24 +0100

    Add files via upload

---
+++ subtask2.py
@@ -0,0 +1,22 @@
+def naturalNB():
+    n=0 #counter
+    s=0 #sum
+    m=None #minimum
+    while True:
+        nb=int(input("Enter a natural number (-1 to stop):"))
+        if nb==-1:
+            break
+        n+=1
+        s+=nb
+        if m is None or nb<m:
+            m=nb
+if n==0:
+       m=-1
+       a=-1
+else:
+       a=s/n
+print("n= ",n)
+print("s= ",s)
+print("m= ",m)
+print("a= ",a)
+naturalNb()
